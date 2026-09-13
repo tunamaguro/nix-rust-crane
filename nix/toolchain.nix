5c678c005215a7d211eff8fdb85c31c8a83eef4e
tunamaguro <79092292+tunamaguro@users.noreply.github.com>
2026-09-13 23:36:38 +0900

    pass only Rust toolchain factory

--- nix/toolchain.nix
+++ nix/toolchain.nix
@@ -4,10 +4,8 @@ let
   rustToolchainFor =
     p:
     (inputs.rust-overlay.lib.mkRustBin { } p).fromRustupToolchainFile ../rust-toolchain.toml;
-
-  rustToolchain = rustToolchainFor pkgs;
 in
 pkgs.callPackage ./build.nix {
   crane = inputs.crane.mkLib pkgs;
-  inherit rustToolchain rustToolchainFor;
+  inherit rustToolchainFor;
 }
